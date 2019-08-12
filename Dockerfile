61e46f3434d4bc8b5dc5789a8a10e98836ce7b51
Denys Balastryk <denysbalastryk@UK1MAC56.local>
2019-08-12 19:31:54 +0100

    dockerize

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM alpine:3.10
+RUN apk add --no-cache --virtual python3-dev && \
+    apk add --no-cache --update python3 && \
+    pip3 install --upgrade pip setuptools && \
+    pip3 install --upgrade requests==2.8.1
+
+ENV HARVEST_ACCESS_TOKEN ''
+ENV HARVEST_ACCOUNT_ID ''
+ENV TEMPLATE_FILE_NAME 'template.json'
+COPY omnomnom.py .
+COPY $TEMPLATE_FILE_NAME .
+
+# default command: display Ansible version
+CMD ["python3", "omnomnom.py" ]
+
--- omnomnom.py
+++ omnomnom.py
@@ -2,7 +2,6 @@ import json
 import requests
 import logging
 import sys
-import pandas as pd
 from datetime import datetime, timedelta
 import os
 
@@ -13,42 +12,16 @@ log_handler.setFormatter(logging.Formatter(logpattern))
 log.addHandler(log_handler)
 log.setLevel(logging.DEBUG)
 
+TEMPLATE_FILE_NAME = os.environ.get("TEMPLATE_FILE_NAME")
 base_url = "https://api.harvestapp.com/"
-url_add_time_entry = base_url + "v2/time_entries"
 url_get_me = base_url + "/v2/users/me"
 url_my_projects = base_url + "v2/users/%s/project_assignments"
-url_my_entries = base_url + "/v2/time_entries"
+url_my_entries = base_url + "v2/time_entries"
+url_submit = "https://robmarlow.harvestapp.com/daily/review"
 headers = {
-    "User-Agent": "Python Harvest API Sample",
+    "User-Agent": "Python Harvest API",
     "Authorization": "Bearer " + os.environ.get("HARVEST_ACCESS_TOKEN"),
     "Harvest-Account-ID": os.environ.get("HARVEST_ACCOUNT_ID")
-}
-template = {
-    "Mon": {
-        "TECH-011 Devops - BAU Requests": {
-         "Programming": 8
-        }
-    },
-    "Tue": {
-        "TECH-011 Devops - BAU Requests": {
-            "Programming": 8
-        }
-    },
-    "Wed": {
-        "TECH-011 Devops - BAU Requests": {
-            "Programming": 8
-        }
-    },
-    "Thu": {
-        "TECH-011 Devops - BAU Requests": {
-            "Programming": 8
-        }
-    },
-    "Fri": {
-        "TECH-011 Devops - BAU Requests": {
-            "Programming": 8
-        }
-    }
 }
 
 
@@ -86,7 +59,7 @@ def add_time_entry(user_id, project_id, task_id, hours, date):
                         headers=headers,
                         data=data
                         )
-    if rsp.status_code != 200:
+    if rsp.status_code != 201:
         log.error(rsp.text)
         sys.exit(1)
     log.debug(rsp.text)
@@ -94,31 +67,84 @@ def add_time_entry(user_id, project_id, task_id, hours, date):
     return rsp_json
 
 
-def get_task_id(project):
-    return [x for x in project['task_assignments'] if x['task']['name'] == 'Programming'][0]['task']['id']
+def get_task_id(project, task_name):
+    return [x for x in project['task_assignments'] if x['task']['name'] == task_name][0]['task']['id']
+
 
 def get_dates():
     day = datetime.today().strftime("%d/%m/%Y")
     dt = datetime.strptime(day, '%d/%m/%Y')
     start_date = dt - timedelta(days=dt.weekday())
     end_date = start_date + timedelta(days=6)
-    return pd.bdate_range(start=start_date, end=end_date)
+    delta = end_date - start_date  # as timedelta
+    week = [start_date + timedelta(days=x) for x in range(delta.days + 1)]
+    return [x for x in week if x.strftime("%A") not in ['Saturday', 'Sunday']]
+
+
+def get_all_entries(params):
+    rsp = requests.get(url_my_entries,
+                        headers=headers,
+                        params=params
+                        )
+    if rsp.status_code != 200:
+        log.error(rsp.text)
+        sys.exit(1)
+    rsp_json = json.loads(rsp.content)
+    return rsp_json
+
+
+def remove_entries_if_available(range):
+    params = {"from": range[0].strftime("%Y-%m-%d"),
+              "to": range[4].strftime("%Y-%m-%d")}
+    time_entries = get_all_entries(params)
+    for entry in time_entries['time_entries']:
+        rsp = requests.delete(url_my_entries + '/%s' % (entry['id']),
+                              headers=headers
+                              )
+        if rsp.status_code != 200:
+            log.error(rsp.text)
+            sys.exit(1)
+        log.debug(rsp.text)
+
+
+def submit_weekly_report(user_id, dates):
+    data = {"authenticity_token": "T___T",
+            "submitted_date": dates[4].timetuple().tm_yday,
+            "of_user": user_id,
+            "submitted_date_year": dates[4].strftime("%Y"),
+            "period_begin": dates[0].timetuple().tm_yday,
+            "period_begin_year": dates[0].strftime("%Y"),
+            "from_screen": "daily",
+            "from_timesheet_beta": "true",
+            "return_to": "/time/day/%s/%s/%s/%s" % (dates[0].strftime("%Y"), dates[0].strftime("%m"),
+                                                    dates[0].strftime("%d"), user_id)
+            }
+    rsp = requests.post(url_submit,
+                        headers=headers,
+                        data=data
+                        )
+    if rsp.status_code != 200:
+        log.error(rsp.text)
+        sys.exit(1)
 
 def main():
-    work_dates = get_dates()
     user_id = get_user_id()
-    for dates, timesheets in zip(work_dates, template.items()):
-        for day in timesheets:
-            project = get_project_id(user_id, "TECH-011 Devops - BAU Requests")
+    work_dates = get_dates()
+    remove_entries_if_available(work_dates)
+    with open(TEMPLATE_FILE_NAME, 'r') as json_file:
+        template = json.load(json_file)
+    for date in work_dates:
+        weekday_name = date.strftime("%A")
+        workday_timeschedule = template[weekday_name]
+        for project_name, tasks in workday_timeschedule.items():
+            project = get_project_id(user_id, project_name)
             project_id = project['project']['id']
-            task_id = get_task_id(project)
-            hours = 8.0
-            date = dates._date_repr
-            add_time_entry(user_id, project_id, task_id, hours, date)
-
-
-# rsp_json = json.loads(rsp.content)
-# print(json.dumps(rsp_json, sort_keys=True, indent=4))
+            for task, time in tasks.items():
+                task_id = get_task_id(project, task)
+                hours = float(time)
+                work_date = date._date_repr
+                add_time_entry(user_id, project_id, task_id, hours, work_date)
+    submit_weekly_report(user_id, work_dates)
 
 
 if __name__ == "__main__":
